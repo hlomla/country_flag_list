ceae6952f1ffd629f4fe0bbd19b1c73d35558dec
hlomla <hlomla.tapuko@gmail.com>
2022-03-08 10:24:09 +0200

    test still failing

--- test/country_flag_test.js
+++ test/country_flag_test.js
@@ -2,16 +2,30 @@ describe('Country Flag List Factory Function', function () {
 
     describe('Return country value when user enters a country', function () {
         it('should be able to return country and its flag', function () {
-            let country = countryFlags();
+            let countries = countryFlags();
 
-            country.countryEntered('country');
-            country.flagInserted('flag')
+            countries.countryEntered('country');
+            countries.flagInserted('flag')
 
-            country.getList(['Argentina','🇦🇷'])
+            let list = countries.getList(['Argentina','🇦🇷']);
 
-            assert.deepEqual(['Argentina','🇦🇷'], country.getList(['Argentina','🇦🇷']))
+            assert.deepEqual(list,  {
+                country: 'Argentina',
+                flag: '🇦🇷'
+            })
 
         });
+        // it('should be able to return country and its flag when two countries have been entered', function () {
+        //     let country = countryFlags();
+
+        //     country.countryEntered('country');
+        //     country.flagInserted('flag')
+
+        //     country.getList(['Argentina','🇦🇷'])
+
+        //     assert.deepEqual(['Argentina','🇦🇷'], country.getList(['Argentina','🇦🇷']))
+
+        // });
     });
 })
 
